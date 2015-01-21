082392b1dde24a3cf550b9067b4ea62eacb364d1
falood <falood@gmail.com>
2015-01-21 23:02:00 +0800

    upgrade vsn

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EIO.Mixfile do
 
   def project do
     [ app: :eio,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.0",
       deps: deps,
       description: "Elixir Server of engine.io",
